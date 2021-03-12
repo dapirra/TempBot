adbcd4229a2c34fcb6dd0b873b9914cea4f2a256
dapirra <35818038+dapirra@users.noreply.github.com>
2021-03-12 08:20:00 -0500

    Rewrote a majority of the TestHardware code

--- TestHardware.py
+++ TestHardware.py
@@ -1,23 +1,66 @@
+import sys
 import wmi
 
-w = wmi.WMI(namespace=r'root\OpenHardwareMonitor')
-temperature_infos = w.Sensor()
-hardware = w.Hardware()
+"""
+This file is used to test if hardware information can be properly read in from Open Hardware Monitor.
+"""
 
-for device in hardware:
-    print(device.Name)
+hardware_read_failure = False
+sensor_read_failure = False
+horizonal_line = '-' * 72
 
-print('-' * 25)
-list.sort(temperature_infos)
-for sensor in temperature_infos:
-    # print(sensor.SensorType, sensor.Name, sensor.Value, sep=': ')
-    if sensor.SensorType == 'Temperature':
-        print(sensor.Name)
-        print(sensor.Value)
+print('Test Initiated.')
 
-print('-' * 25)
+try:
+    w = wmi.WMI(namespace=r'root\OpenHardwareMonitor')
+except:
+    print('Error: creating WMI object.')
+    sys.exit(1)
 
-# Powershell command: Get-WmiObject MSAcpi_ThermalZoneTemperature -Namespace "root/wmi"
+try:
+    hardware_info = w.Hardware()
+except:
+    print('Error: reading Hardware info.')
+    sys.exit(1)
+
+try:
+    sensor_info = w.Sensor()
+except:
+    print('Error: reading Sensor info.')
+    sys.exit(1)
+
+if not hardware_info:
+    hardware_read_failure = True
+    print('Error: Hardware request is null.')
+
+if not sensor_info:
+    sensor_read_failure = True
+    print('Error: Sensor request is null.')
+
+if not hardware_read_failure:
+    list.sort(hardware_info)
+
+    print(horizonal_line)
+    print('Printing all Hardware Information:')
+
+    for device in hardware_info:
+        print('{:<20}{:<30}'.format(device.HardwareType, device.Name))
+
+if not sensor_read_failure:
+    list.sort(sensor_info)
+
+    print(horizonal_line)
+    print('Printing all Sensor Information:')
+    print('{:<20}{:<20}{:<20}'.format('Name', 'Sensor Type', 'Value'))
+    for sensor in sensor_info:
+        print('{:<20}{:<20}{:<20}'.format(sensor.Name, sensor.SensorType, sensor.Value))
+
+    print(horizonal_line)
+
+if hardware_read_failure and sensor_read_failure:
+    print('Open Hardware Monitor is probably not running')
+
+# Powershell command: Get-WmiObject MSAcpi_ThermalZoneTemperature -Namespace 'root/wmi"
 
 # Print temperature with powershell
 # w = wmi.WMI(namespace=r'root\wmi')
